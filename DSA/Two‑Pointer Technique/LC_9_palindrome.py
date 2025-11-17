d84cc1d5b569db15f9c88a5036bcea4102417bdb
MD Akeel <aakiltayyab@gmail.com>
2025-11-17 19:15:20 +0530

    Add : LeetCode Question-9, isPalindrome

---
+++ DSA/Two‑Pointer Technique/LC_9_palindrome.py
@@ -0,0 +1,14 @@
+# LeetCode - 9 : Palindrome
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Convert integer to string for easy indexing
+        x_str = str(x)
+        for i in range(len(x_str) // 2):
+            if x_str[i] != x_str[len(x_str) - i - 1]:
+                return False
+        return True
+
+
+# Test
+print(Solution().isPalindrome(121))   # ✅ True
+print(Solution().isPalindrome(123))   # ❌ False
